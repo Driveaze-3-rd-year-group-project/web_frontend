d7c566499db9cefc0970018499cfb4dc0179e1f1
iamgian2001 <ggogamer60@gmail.com>
2024-07-17 23:05:58 +0530

    Update feedback.jsx

--- driveaze/src/components/userpage/Customer/feedback.jsx
+++ driveaze/src/components/userpage/Customer/feedback.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { FaCircle } from "react-icons/fa";
-import { useLocation } from 'react-router-dom';
 import { MdReport } from "react-icons/md";
 import { VscFeedback } from "react-icons/vsc";
 
@@ -8,11 +6,11 @@ const Feedback = () => {
 
   const members = [
     {
-      icon: <VscFeedback size="40" className="text-dered"/>,
+      icon: <VscFeedback size="40" className="text-dered" />,
       title: "Send us your thoughts",
-    }, 
+    },
     {
-      icon: <MdReport size="40"  className="text-dered"/>,
+      icon: <MdReport size="40" className="text-dered" />,
       title: "Report a issue",
     }
   ];
@@ -67,10 +65,28 @@ const Feedback = () => {
 
       {/* Popup component (conditionally rendered) */}
       {isOpen && selectedMember && (
-        <div className="fixed w-full h-full top-0 left-0  bg-gray-200 bg-opacity-50 z-50 flex justify-center items-center">
+        <div className="fixed w-full h-full top-0 left-0 bg-gray-200 bg-opacity-50 z-50 flex justify-center items-center">
           <div className="bg-deepblue w-1/2 h-38 p-4 rounded-lg shadow-md" ref={popupRef}>  {/* Set minimum width & height for popup */}
             <h2 className="p-5 text-white text-xl font-semibold mb-4">{selectedMember.title}</h2>
-            <textarea className="w-full h-48 border rounded p-2" placeholder="Enter your feedback"></textarea>  {/* Increased textarea height */}
+
+            {/* Conditionally render the subject input */}
+            {selectedMember.title === "Report a issue" && (
+              <div className="max-w-md px-4 mx-auto mt-4">
+                <label htmlFor="subject" className="block text-white py-1 text-gray-500 mb-2">
+                  Subject
+                </label>
+                <div className="flex items-center text-gray-400 border rounded-md mt-4 mb-4">
+                  <input
+                    type="text"
+                    placeholder="Subject"
+                    id="subject"
+                    className="w-full h-1/2 p-2 bg-white text-black border-2 border-black"
+                  />
+                </div>
+              </div>
+            )}
+
+            <textarea className="w-full h-48 border rounded p-2" placeholder="Type.."></textarea>  {/* Increased textarea height */}
             <div className="flex justify-end mt-4">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClosePopup}>Send</button>
             </div>

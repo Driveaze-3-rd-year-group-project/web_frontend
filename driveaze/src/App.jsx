21e08ba8b07c3447ab88ed34547d440b761473d1
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2024-07-18 13:02:36 +0530

    Job management pages done

--- driveaze/src/App.jsx
+++ driveaze/src/App.jsx
@@ -1,27 +1,25 @@
-
-import React from 'react';
-import Landingpage from './components/userpage/Landingpage';
+import React from "react";
+import Landingpage from "./components/userpage/Landingpage";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from './components/auth/LoginPage';
-import RegistrationPage from './components/auth/RegistrationPage';
-import UserService from './components/service/UserService';
-import ProtectedRoute from './components/ProtectedRoute';
-import Sidebar from './components/common/Sidebar';
-import AdminDashboard from './components/userpage/Admin/AdminDashboard';
-import StaffAccounts from './components/userpage/Admin/StaffAccounts';
-import SupervisorDashboard from './components/userpage/Supervisor/SupervisorDashboard';
-import CustomerDashboard from './components/userpage/Customer/CustomerDashboard';
+import LoginPage from "./components/auth/LoginPage";
+import RegistrationPage from "./components/auth/RegistrationPage";
+import UserService from "./components/service/UserService";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Sidebar from "./components/common/Sidebar";
+import AdminDashboard from "./components/userpage/Admin/AdminDashboard";
+import StaffAccounts from "./components/userpage/Admin/StaffAccounts";
+import SupervisorDashboard from "./components/userpage/Supervisor/SupervisorDashboard";
+import CustomerDashboard from "./components/userpage/Customer/CustomerDashboard";
 
-import ReceptionistDashboard from './components/userpage/Receptionist/ReceptionistDashboard';
-import JobManagement from './components/userpage/Receptionist/JobManagement';
-import VehicleManagement from './components/userpage/Receptionist/VehicleManagement';
-
-import CustomerAccounts from './components/userpage/Admin/CustomerAccounts';
-import BookingManagement from './components/userpage/Receptionist/BookingManagement';
-import Payments from './components/userpage/Receptionist/Payments';
-import Billing from './components/userpage/Receptionist/Billing';
-import EditVehicle from './components/userpage/Receptionist/EditVehicle';
-
+import ReceptionistDashboard from "./components/userpage/Receptionist/ReceptionistDashboard";
+import JobManagement from "./components/userpage/Receptionist/JobManagement";
+import VehicleManagement from "./components/userpage/Receptionist/VehicleManagement";
+import BookingManagement from "./components/userpage/Receptionist/BookingManagement";
+import Payments from "./components/userpage/Receptionist/Payments";
+import Billing from "./components/userpage/Receptionist/Billing";
+import EditVehicle from "./components/userpage/Receptionist/EditVehicle";
+import JobDetails from "./components/userpage/Receptionist/JobDetails";
+import JobCreate from "./components/userpage/Receptionist/JobCreate";
 
 function App() {
   return (
@@ -32,7 +30,11 @@ function App() {
             <Sidebar />
           </div>
         )}
-        <div className={`content ${UserService.isAuthenticated() ? 'w-3/4' : 'w-full'}`}>
+        <div
+          className={`content ${
+            UserService.isAuthenticated() ? "w-3/4" : "w-full"
+          }`}
+        >
           <Routes>
             {!UserService.isAuthenticated() && (
               <>
@@ -54,20 +56,28 @@ function App() {
                 <>
                   <Route path="/dashboard" element={<AdminDashboard />} />
                   <Route path="/staffaccounts" element={<StaffAccounts />} />
-                  <Route path="/customeraccounts" element={<CustomerAccounts />} />
                   <Route path="*" element={<Navigate to="/dashboard" />} />
                 </>
               )}
               {!UserService.isCustomer() ? (
                 <>
                   <Route path="/profile" element={<Navigate to="/" />} />
-                  <Route path="/update-user/:userId" element={<Navigate to="/profile" />} />
+                  <Route
+                    path="/update-user/:userId"
+                    element={<Navigate to="/profile" />}
+                  />
                 </>
               ) : (
                 <>
                   <Route path="/dashboard" element={<CustomerDashboard />} />
-                  <Route path="/admin/user-management" element={<Navigate to="/profile" />} />
-                  <Route path="/update-user/:userId" element={<Navigate to="/profile" />} />
+                  <Route
+                    path="/admin/user-management"
+                    element={<Navigate to="/profile" />}
+                  />
+                  <Route
+                    path="/update-user/:userId"
+                    element={<Navigate to="/profile" />}
+                  />
                 </>
               )}
               {!UserService.isSupervisor() ? (
@@ -77,30 +87,59 @@ function App() {
               ) : (
                 <>
                   <Route path="/dashboard" element={<SupervisorDashboard />} />
-                  <Route path="/admin/user-management" element={<Navigate to="/profile" />} />
-                  <Route path="/update-user/:userId" element={<Navigate to="/profile" />} />
+                  <Route
+                    path="/admin/user-management"
+                    element={<Navigate to="/profile" />}
+                  />
+                  <Route
+                    path="/update-user/:userId"
+                    element={<Navigate to="/profile" />}
+                  />
                 </>
               )}
               {!UserService.isReceptionist() ? (
                 <>
-                  <Route path="/jobmanagement" element={<Navigate to="/dashboard" />} />
-                  <Route path="/vehiclemanagement" element={<Navigate to="/dashboard" />} />
-                  <Route path="/bookingmanagement" element={<Navigate to="/dashboard" />} />
-                  <Route path="/billing" element={<Navigate to="/dashboard" />} />
-                  <Route path="/payments" element={<Navigate to="/dashboard" />} />
+                  <Route
+                    path="/jobmanagement"
+                    element={<Navigate to="/dashboard" />}
+                  />
+                  <Route
+                    path="/vehiclemanagement"
+                    element={<Navigate to="/dashboard" />}
+                  />
+                  <Route
+                    path="/bookingmanagement"
+                    element={<Navigate to="/dashboard" />}
+                  />
+                  <Route
+                    path="/billing"
+                    element={<Navigate to="/dashboard" />}
+                  />
+                  <Route
+                    path="/payments"
+                    element={<Navigate to="/dashboard" />}
+                  />
                 </>
               ) : (
                 <>
-                  <Route path="/dashboard" element={<ReceptionistDashboard />} />
+                  <Route
+                    path="/dashboard"
+                    element={<ReceptionistDashboard />}
+                  />
                   <Route path="/jobmanagement" element={<JobManagement />} />
-
-                  <Route path="/vehiclemanagement" element={<VehicleManagement />} />          
-=======
-                  <Route path="/vehiclemanagement" element={<VehicleManagement />} />
-                  <Route path="/bookingmanagement" element={<BookingManagement />} />
+                  <Route path="/jobcreate" element={<JobCreate />} />
+                  <Route path="/jobdetails" element={<JobDetails />} />
+                  <Route
+                    path="/vehiclemanagement"
+                    element={<VehicleManagement />}
+                  />
+                  <Route path="/editvehicle" element={<EditVehicle />} />
+                  <Route
+                    path="/bookingmanagement"
+                    element={<BookingManagement />}
+                  />
                   <Route path="/billing" element={<Billing />} />
-                  <Route path="/payments" element={<Payments />} />  
-                  <Route path="/editvehicle" element={<EditVehicle />} /> 
+                  <Route path="/payments" element={<Payments />} />
                   <Route path="*" element={<Navigate to="/dashboard" />} />
                 </>
               )}
---
+++ driveaze/src/components/userpage/Receptionist/JobCreate.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const JobCreate = () => {
+  return (
+    <main className="py-14">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="max-w-lg mx-auto space-y-3 sm:text-center">
+          <h3 className="text-indigo-600 font-semibold">Create New Job</h3>
+        </div>
+        <div className="mt-12 max-w-lg mx-auto">
+          <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+            <div>
+              <label className="font-medium">Vehicle Number</label>
+              <input
+                type="text"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Job Started Date</label>
+              <input
+                type="date"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Assigned Supervisor</label>
+              <input
+                type="text"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium">
+                Service Request(Details given by Customer)
+              </label>
+              <textarea
+                required
+                className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              ></textarea>
+            </div>
+            <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default JobCreate;

2e9baa337e8106ef38966c1c2900bf8e313af610
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2024-07-19 09:18:52 +0530

    changes done in job and vehicle management

--- driveaze/src/App.jsx
+++ driveaze/src/App.jsx
@@ -23,6 +23,7 @@ import JobDetails from "./components/userpage/Receptionist/JobDetails";
 import JobCreate from "./components/userpage/Receptionist/JobCreate";
 import RepairVehicles from "./components/userpage/Supervisor/ReapairVehicles";
 import CustomerAccounts from "./components/userpage/Admin/CustomerAccounts";
+import AddVehicle from "./components/userpage/Receptionist/AddVehicle";
 
 function App() {
   return (
@@ -105,6 +106,7 @@ function App() {
                   <Route path="/bookingmanagement" element={<BookingManagement />} />
                   <Route path="/billing" element={<Billing />} />
                   <Route path="/payments" element={<Payments />} />
+                  <Route path="/addvehicle" element={<AddVehicle />} />
                   <Route path="*" element={<Navigate to="/dashboard" />} />
                 </>
               )}
--- driveaze/src/components/userpage/Receptionist/JobCreate.jsx
+++ driveaze/src/components/userpage/Receptionist/JobCreate.jsx
@@ -42,9 +42,20 @@ const JobCreate = () => {
                 className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
               ></textarea>
             </div>
-            <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
-              Save
-            </button>
+            <div className="flex justify-between items-center mt-6">
+              <a
+                href="/jobmanagement"
+                className="px-8 py-3 text-white font-medium bg-red-500 hover:bg-red-400 active:bg-red-600 rounded-lg duration-150"
+              >
+                Back
+              </a>
+              <button
+                type="submit"
+                className="px-8 py-3 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg duration-150"
+              >
+                Save
+              </button>
+            </div>
           </form>
         </div>
       </div>

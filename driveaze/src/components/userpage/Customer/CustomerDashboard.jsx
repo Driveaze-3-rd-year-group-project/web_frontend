3cb16d17254e412419e843db7d46c0c5a4978420
iamgian2001 <ggogamer60@gmail.com>
2024-07-21 12:05:09 +0530

    base

--- driveaze/src/components/userpage/Customer/CustomerDashboard.jsx
+++ driveaze/src/components/userpage/Customer/CustomerDashboard.jsx
@@ -2,21 +2,7 @@ import React from 'react'
 
 const CustomerDashboard = () => {
   return (
-    <div className="flex flex-wrap justify-center">  
-      <Card
-        brand="Toyota"
-        icon={IoCarSportSharp}
-        model="Yaris 2020"
-        buttonText="More Info"
-      />
-      <Card
-        brand="Honda"
-        icon={IoCarSportSharp} // Change the icon if desired
-        model="Civic 2021"
-        buttonText="More Info"
-      />
-
-    </div>
+   <div>Customer Dashborad</div>
   );
 };
 

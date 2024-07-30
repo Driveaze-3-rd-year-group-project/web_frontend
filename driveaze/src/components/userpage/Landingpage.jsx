90e82f33a5406f548afe75085eb6da921341106d
Virajith Dissanayaka <121111846+virajithdissanayaka@users.noreply.github.com>
2024-07-30 21:33:49 +0530

    Landing page update

--- driveaze/src/components/userpage/Landingpage.jsx
+++ driveaze/src/components/userpage/Landingpage.jsx
@@ -1,41 +1,99 @@
-import { useEffect, useState } from 'react'
-import UserService from '../service/UserService';
+import React, { useState, useEffect } from "react";
+import { ChevronLeft, ChevronRight } from "react-feather";
+
+import img1 from "../../assets/LandingPageCarousel/1.svg";
+import img2 from "../../assets/LandingPageCarousel/2.svg";
+import img3 from "../../assets/LandingPageCarousel/3.svg";
+import img4 from "../../assets/LandingPageCarousel/4.svg";
 
 const Landingpage = () => {
+    const [curr, setCurr] = useState(0);
+
+    const slides = [
+        img1,
+        img2,
+        img3,
+        img4
+    ];
+
+    const prev = () => setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
+    const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+
+    const autoSlide = true;
+    const autoSlideInterval = 3000;
+
+    useEffect(() => {
+        if (!autoSlide) return;
+        const slideInterval = setInterval(next, autoSlideInterval);
+        return () => clearInterval(slideInterval);
+    }, [autoSlide, autoSlideInterval]);
+
     return (
         <>
-        <style>{`
-            body::-webkit-scrollbar {
-    display: none;
-}
-        `}</style>
-        <section className="h-[calc(100vh-96px)] mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
-            <div className="space-y-4 flex-1 sm:text-center lg:text-left">
-                <div>
-                    <h1 className='font-bold text-4xl xl:text-5xl'><span className="text-driveazered"> SAMARASINGHE </span><span className='text-deepblue'> Motors</span></h1> 
-                    <h2 className='text-black font-bold text-3xl  xl:text-4xl'>The name you can trust</h2>
+            <style>
+                {`
+                    body::-webkit-scrollbar {
+                        display: none;
+                    }
+                `}
+            </style>
+            <section className="h-[calc(100vh-96px)] mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
+                <div className="space-y-4 flex-1 sm:text-center lg:text-left">
+                    <div>
+                        <h1 className='font-bold text-4xl xl:text-5xl'>
+                            <span className="text-driveazered">SAMARASINGHE</span>
+                            <span className='text-deepblue'> MOTORS </span>
+                        </h1>
+                        <h2 className='text-black font-bold text-3xl xl:text-4xl'>The name you can trust</h2>
+                    </div>
+                    <p className="text-black max-w-xl leading-relaxed sm:mx-auto lg:ml-0 text-justify">
+                        Welcome to Samarasinghe Motors Pvt Ltd, the premier service station in Matara area over 65 years of trusted service, renowned for its excellence and large-scale operations. With a dedicated team of skilled professionals, we specialize in servicing all types of Japanese hybrid petrol and electric vehicles, ensuring top-notch care for your car. Join our extensive customer base and experience the superior service that has made us a trusted name in the industry.
+                    </p>
+                    <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
+                        <a href="/login" className="px-7 py-3 w-full  text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 text-center rounded-md block sm:w-auto">
+                            Book your service
+                        </a>
+                    </div>
                 </div>
-                <p className="text-black max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
-                Samarasinghe Motors Ltd. is one of the pioneers and largest automobile service company in Southern Region of Sri Lanka based in Matara.
-                <br/>
-                <br/>
-                The company offers a wide range of services for almost all purposes, since the year 1955.  Over the years we have built our reputation through extending trustworthiness and high quality services to our clients. Our prominence on high quality and customer satisfaction service has placed us with the high growth of clients.
-                </p>
-                <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
-                    <a href="javascript:void(0)" className="px-7 py-3 w-full bg-white text-gray-800 text-center rounded-md shadow-md block sm:w-auto">
-                        Get started
-                    </a>
-                    <a href="javascript:void(0)" className="px-7 py-3 w-full bg-gray-700 text-gray-200 text-center rounded-md block sm:w-auto">
-                        Try it out
-                    </a>
+
+                <div className="overflow-hidden relative shadow-lg rounded-lg">
+                    <div
+                        className="flex transition-transform ease-in-out duration-500 w-120"
+                        style={{ transform: `translateX(-${curr * 100}%)` }}
+                    >
+                        {slides.map((slide, index) => (
+                            <img key={index} src={slide} alt={`Slide ${index}`} className="w-full" />
+                        ))}
+                    </div>
+                    <div className="absolute inset-0 flex items-center justify-between p-4">
+                        <button
+                            onClick={prev}
+                            className="p-1 rounded-full shadow text-gray-800 hover:bg-white"
+                        >
+                            <ChevronLeft size={40} />
+                        </button>
+                        <button
+                            onClick={next}
+                            className="p-1 rounded-full shadow  text-gray-800 hover:bg-white"
+                        >
+                            <ChevronRight size={40} />
+                        </button>
+                    </div>
+                    <div className="absolute bottom-4 right-0 left-0">
+                        <div className="flex items-center justify-center gap-2">
+                            {slides.map((_, i) => (
+                                <div
+                                    key={i}
+                                    className={`
+                                        transition-all w-3 h-3 bg-white rounded-full
+                                        ${curr === i ? "p-2" : "bg-opacity-50"}
+                                    `}
+                                />
+                            ))}
+                        </div>
+                    </div>
                 </div>
-            </div>
-            //carousel
-            
-            <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
-                <img src="https://i.postimg.cc/HxHyt53c/undraw-heatmap-uyye.png" className="w-full mx-auto sm:w-10/12  lg:w-full" />
-            </div>
-        </section>
+            </section>
         
         <section class="bg-deepblue ">
             <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
@@ -85,67 +143,15 @@ const Landingpage = () => {
             </div>
         </section>
         <section class="bg-gray-900">
-            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-                <div class="mb-8 max-w-screen-md lg:mb-16">
-                    <h2 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-                    <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-                </div>
-                <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                    <div>
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <h3 class="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
-                    </div>
-                    <div>
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
-                        </div>
-                        <h3 class="mb-2 text-xl font-bold dark:text-white">Legal</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
-                    </div>
-                    <div>
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
-                        </div>
-                        <h3 class="mb-2 text-xl font-bold dark:text-white">Business Automation</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.</p>
-                    </div>
-                    <div>
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <h3 class="mb-2 text-xl font-bold dark:text-white">Finance</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Audit-proof software built for critical financial operations like month-end close and quarterly budgeting.</p>
-                    </div>
-                    <div>
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
-                        </div>
-                        <h3 class="mb-2 text-xl font-bold dark:text-white">Enterprise Design</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Craft beautiful, delightful experiences for both marketing and product with real cross-company collaboration.</p>
-                    </div>
-                    <div>
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <h3 class="mb-2 text-xl font-bold dark:text-white">Operations</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Keep your company’s lights on with customizable, iterative, and structured workflows built for all efficient teams and individual.</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="bg-white dark:bg-gray-900">
             <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-                <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                    <h2 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
-                    <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-                    <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                <div class="font-light text-white sm:text-lg">
+                    <h1 class="mb-8 text-4xl font-extrabold text-white">About Us</h1>
+                    <p class="mb-4 max-w-xl leading-relaxed sm:mx-auto lg:ml-0 text-justify">Samarasinghe Motors Ltd. is one of the pioneers and largest automobile service company in Southern Region of Sri Lanka based in Matara.</p>
+                    <p class="mb-4 max-w-xl leading-relaxed sm:mx-auto lg:ml-0 text-justify">The company offers a wide range of services for almost all purposes, since the year 1955. Over the years we have built our reputation through extending trustworthiness and high quality services to our clients. Our prominence on high quality and customer satisfaction service has placed us with the high growth of clients.</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 mt-8">
-                    <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1"/>
-                    <img class="mt-4 w-full rounded-lg lg:mt-10" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2"/>
+                    <img src="./src/assets/LandingPageAboutUs/about_us1.svg" class="w-full rounded-lg"  alt="office content 1"/>
+                    <img class="mt-4 w-full rounded-lg lg:mt-10" src="./src/assets/LandingPageAboutUs/about_us2.svg" alt="office content 2"/>
                 </div>
             </div>
         </section>
@@ -153,7 +159,7 @@ const Landingpage = () => {
             <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
                     <h2 class="mb-4 text-4xl font-bold text-gray-900 dark:text-white">Powering innovation at <span class="font-extrabold">200,000+</span> companies worldwide</h2>
-                    <p class="mb-4 font-light">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                    <p class="mb-4 font-light ">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
                     <p class="mb-4 font-medium">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
                     <a href="#" class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
                         Learn more

b8fb32226a6988f51d7fb9ee9bae06677f2637b5
SAKKalinduS <sankalpa5285@gmail.com>
2024-11-23 23:59:04 +0530

    WarehouseKeeper dashboard

--- driveaze/src/components/common/Sidebar.jsx
+++ driveaze/src/components/common/Sidebar.jsx
@@ -208,41 +208,6 @@ function Sidebar() {
                                         </div>
                                         <p className="flex-1">Staff Accounts</p>
                                     </a>
-                                    <a href="/customeraccounts" className="flex items-center gap-x-2 text-white p-2 rounded-lg  hover:bg-lightblue hover:text-black active:bg-gray-100 duration-150">
-                                        <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
-                                        <div className="flex-none">
-                                            <FaUsers />
-                                        </div>
-                                        <p className="flex-1">Customer Accounts</p>
-                                    </a>
-                                    <a href="/ongoingjobs" className="flex items-center gap-x-2 text-white p-2 rounded-lg  hover:bg-lightblue hover:text-black active:bg-gray-100 duration-150">
-                                        <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
-                                        <div className="flex-none">
-                                            <FaBusinessTime />
-                                        </div>
-                                        <p className="flex-1">Ongoing Jobs</p>
-                                    </a>
-                                    <a href="/registeredvehicles" className="flex items-center gap-x-2 text-white p-2 rounded-lg  hover:bg-lightblue hover:text-black active:bg-gray-100 duration-150">
-                                        <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
-                                        <div className="flex-none">
-                                            <FaCarAlt />
-                                        </div>
-                                        <p className="flex-1">Registered Vehicles</p>
-                                    </a>
-                                    <a href="/customercomplaints" className="flex items-center gap-x-2 text-white p-2 rounded-lg  hover:bg-lightblue hover:text-black active:bg-gray-100 duration-150">
-                                        <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
-                                        <div className="flex-none">
-                                            <FaRegFrown />
-                                        </div>
-                                        <p className="flex-1">Customer Complaints</p>
-                                    </a>
-                                    <a href="/reports" className="flex items-center gap-x-2 text-white p-2 rounded-lg  hover:bg-lightblue hover:text-black active:bg-gray-100 duration-150">
-                                        <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
-                                        <div className="flex-none">
-                                            <FaChartLine />
-                                        </div>
-                                        <p className="flex-1">Reports & Analytics</p>
-                                    </a>
                                     <a href="/siteannouncements" className="flex items-center gap-x-2 text-white p-2 rounded-lg  hover:bg-lightblue hover:text-black active:bg-gray-100 duration-150">
                                         <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
                                         <div className="flex-none">

89a4f95cbc31accd49c8f2acd7d8dedab009d7c6
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2024-12-02 11:35:34 +0530

    Addservice form added

--- driveaze/src/components/common/Sidebar.jsx
+++ driveaze/src/components/common/Sidebar.jsx
@@ -184,8 +184,8 @@ function Sidebar() {
                                         </div>
                                         <p className="flex-1">Customer Complaints</p>
                                     </a>
-                                    <a href="/reports"  className={`flex items-center gap-x-2 p-2 rounded-lg duration-150 hover:bg-lightblue hover:text-black
-                                         ${getTabStyles('reports')}`}>
+                                    <a href="/servicetypes"  className={`flex items-center gap-x-2 p-2 rounded-lg duration-150 hover:bg-lightblue hover:text-black
+                                         ${getTabStyles('servicetypes')}`}>
                                         <div className="absolute left-0 w-1.5 h-full rounded-r-full bg-gray-600 scale-y-0 group-hover:scale-y-100 transition-transform ease-in-out" />
                                         <div className="flex-none">
                                             <FaChartLine />

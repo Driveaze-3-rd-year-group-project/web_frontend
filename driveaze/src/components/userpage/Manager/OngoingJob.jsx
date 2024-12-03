b0d5cf3691012acc9a6b40973314a8b6cc3f6e5e
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2024-12-03 14:28:06 +0530

    ongoingjobs search bar

--- driveaze/src/components/userpage/Manager/OngoingJob.jsx
+++ driveaze/src/components/userpage/Manager/OngoingJob.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import SupervisorService from '../../service/SupervisorService';
+import { FaSearch } from 'react-icons/fa';
 
 const OngoingJob = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -44,34 +45,31 @@ const OngoingJob = () => {
 
   return (
     <div className="mt-14 max-w-screen-xl mx-auto px-4 md:px-8">
-      <div className="items-start justify-between md:flex">
-        <div className="max-w-lg">
-          <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
-            Repairing Vehicle List
-          </h3>
-        </div>
-        <div className="mt-3 md:mt-0 flex items-center">
-          <form
-            onSubmit={(e) => e.preventDefault()} 
-            className="max-w-md px-4 mx-auto mt-12"
-          >
-            <div className="relative">
-              <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-
-              <input
-                type="text"
-                value={searchTerm}
-                onChange={handleSearchChange}
-                placeholder="Search jobs..."
-                className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
-              />
-            </div>
-          </form>
-        </div>
+      {/* Title */}
+      <div className="flex justify-between items-center mb-4">
+        <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
+          Registered Vehicles
+        </h3>
       </div>
-      <ul className="mt-12 divide-y">
+
+      {/* Search and Add Button */}
+      <div className="flex justify-between items-center mb-3">
+        {/* Search Bar */}
+        <form onSubmit={(e) => e.preventDefault()} className="flex">
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="py-2 px-3 pr-10 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+            {/* Search Icon */}
+            <FaSearch className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-400" />
+          </div>
+        </form>
+      </div>
+      <ul className="mt-4 divide-y">
         {jobs
           .filter((item) =>
             item.numberPlate.toLowerCase().includes(searchTerm.toLowerCase()) ||
